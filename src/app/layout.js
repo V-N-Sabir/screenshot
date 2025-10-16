4af14af976923d7dd64352d1644291427d2e3947
Sabir <sabirka682@gmail.com>
2025-10-16 10:13:49 +0500

    update image to png

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import imgMeta from "../component/image/metaimage.jpg";
+import imgMeta from "../component/image/metaimage.png";
 import "./globals.css";
 
 
